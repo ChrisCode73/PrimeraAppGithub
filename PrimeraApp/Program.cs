2952552b77c31baf7eba593b9563e90fcdcf2098
ingcm <ingcm@RT_LAB>
2024-08-29 14:21:59 -0500

    Agregar archivos de proyecto.

---
+++ PrimeraApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeraApp
+{
+    internal class Program // Esta es una clase propia.
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.WriteLine("bye");
+        }
+    }
+}
